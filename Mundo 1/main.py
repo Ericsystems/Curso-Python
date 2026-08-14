384814beae76e3b6ea9b48c813cc2baeedb7c3ff
Eric Oliveira Costa <ericsystemsia@gmail.com>
2026-08-14 01:17:33 -0300

    Primeiros passos

---
+++ Mundo 1/main.py
@@ -0,0 +1,20 @@
+# Sempre que quiser imprimir algo na tela use aspas, seja ela simples ('') ou duplas ("")
+print('Olá, Mundo!')
+
+# Numeros sao usados primordialmente para fazer calculos dentro do programa, também é possivel  imprimir o numero na tela sem fazer calculo ao adicionar ele dentro de aspas (''). Nessa ocasião,basicamente as aspas sao usadas para transformar numero em texto.
+
+print(7+2, ', Imprime na tela a soma entre 7 e 2')
+print('7+2, nao imprime a soma pois os numeros estão inicialmente entre aspas')
+
+# Se quero juntar 2 mensagens dentro da mesma chamada, é possivel com a estrutura 
+# print('Mensagem' + 'Mensagem') utilizando o +.
+# Nome, idade, peso, são exemplos de variaveis, toda variavel é um objeto e pode receber valores
+# Qualquer texto fora de aspas deve ser escrito de forma minuscula para nao confundir oprograma.
+# input serve para fazer uma pergunta ao usuario, esse input pode ou deve ser atribuido a uma variavel para receber o 'valor' digitado pelo usuario.
+
+nome = input('Qual é o seu Nome?: ')
+idade = input('Qual a sua Idade?: ')
+peso = input('Qual o seu Peso?: ')
+
+print('Meu nome é ', nome, 'Tenho ', idade, 'anos e peso', peso, 'kg.' )
+
